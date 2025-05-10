516fa9e29f06ec7223e28df21b6aa754cbd23484
marlontamo2 <marlontamo2@gmail.com>
2025-05-10 20:42:57 +0800

    modified about list endpoint used

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
     <h1>This is a frontend application that interacts with the EmployeeAPI, a RESTful service built with ASP.NET Web API.</h1>
+    <strong>Consuming this url's</strong>
+    <ul class="list-group text-left">
+      <li class="list-group-item">(Get/Post)https://localhost:7033/api/EmployeeAPI</li>
+      <li class="list-group-item">(Put/Delete)https://localhost:7033/api/EmployeeAPI/{id}</li>
+      <li class="list-group-item">(Get)https://localhost:7033/api/EmployeeAPI/{id}</li>  
+    </ul>
   </div>
 </template>
